f4e4429c9d17cd4ef7eca3c49b1b837e40a18d2b
djordyolijve <56109252+djordyolijve@users.noreply.github.com>
2021-03-27 21:39:05 +0100

    TicTacToe main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main{
 
@@ -12,6 +13,37 @@ public class Main{
         t1.start();
         t2.start();
 
+        Scanner scan = new Scanner(System.in);
+        TicTacToe game = new TicTacToe();
+        game.bordAanmaken();
+        System.out.println("TicTacToe");
+        do
+        {
+            System.out.println("Hoe het bord er nu uitziet:");
+            game.bordPrinten();
+            int rij;
+            int kol;
+            do
+            {
+                System.out.println("Speler " + game.getSpelerDieSpeelt() + ", Typ een lege rij Verticaal en kolom Horizontaal");
+                rij = scan.nextInt()-1;
+                kol = scan.nextInt()-1;
+            }
+            while (!game.kleurIn(rij, kol));
+            game.beurtDoorgeven();
+        }
+        while(!game.controleerWinnaar() && !game.isHetBordVol());
+        if (game.isHetBordVol() && !game.controleerWinnaar())
+        {
+            System.out.println("Het is gelijkspel");
+        }
+        else
+        {
+            System.out.println("Current board layout:");
+            game.bordPrinten();
+            game.beurtDoorgeven();
+            System.out.println(Character.toUpperCase(game.getSpelerDieSpeelt()) + " Heeft gewonnen");
+        }
     }
 
 }
