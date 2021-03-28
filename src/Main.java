6ce89991faeb1c0c42b9a1358f397bf95a6a482a
Thierry Brouwer <58725453+ThierryBrouwer@users.noreply.github.com>
2021-03-28 14:04:31 +0200

    Gui starten vanuit Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main{
 
-    public static void  main(String[] args){
+    public static void main(String[] args) {
         Connection con = new Connection("Henk");
         Connection con2 = new Connection("Bassie");
 
@@ -12,33 +16,29 @@ public class Main{
 
         t1.start();
         t2.start();
+        new Thread(() -> Application.launch(Gui.class, args)).start();
 
         Scanner scan = new Scanner(System.in);
         TicTacToe game = new TicTacToe();
         game.bordAanmaken();
         System.out.println("TicTacToe");
-        do
-        {
+        do {
             System.out.println("Hoe het bord er nu uitziet:");
             game.bordPrinten();
             int rij;
             int kol;
-            do
-            {
+            do {
                 System.out.println("Speler " + game.getSpelerDieSpeelt() + ", Typ een lege rij Verticaal en kolom Horizontaal");
-                rij = scan.nextInt()-1;
-                kol = scan.nextInt()-1;
+                rij = scan.nextInt() - 1;
+                kol = scan.nextInt() - 1;
             }
             while (!game.kleurIn(rij, kol));
             game.beurtDoorgeven();
         }
-        while(!game.controleerWinnaar() && !game.isHetBordVol());
-        if (game.isHetBordVol() && !game.controleerWinnaar())
-        {
+        while (!game.controleerWinnaar() && !game.isHetBordVol());
+        if (game.isHetBordVol() && !game.controleerWinnaar()) {
             System.out.println("Het is gelijkspel");
-        }
-        else
-        {
+        } else {
             System.out.println("Current board layout:");
             game.bordPrinten();
             game.beurtDoorgeven();
