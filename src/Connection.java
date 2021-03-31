38d2c725ea66b307e38da53e7b10272579e8a468
Geert Perton <hippcityofficial@gmail.com>
2021-03-31 11:27:34 +0200

    connectie klasse heeft (bijna) alle methodes om de server te kunnen bereiken.
    connectie klasse haalt nu server berrichten uit elkaar om de Map of de List uit het server berricht te kunnen halen.

--- src/Connection.java
+++ src/Connection.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Arrays;
 
 public class Connection implements Runnable{
@@ -10,6 +12,9 @@ public class Connection implements Runnable{
     String name;
     Socket s;
     String servermsg;
+    String cleanServermsg;
+    HashMap<String, String> serverMsgHashMap;
+    ArrayList<String> serverMsgArrayList;
 
     PrintWriter pr;
 
@@ -34,15 +39,10 @@ public class Connection implements Runnable{
 
             servermsg = bf.readLine();
 
-
-            String servermsgOld = servermsg;
-
             System.out.println(this.name + ": " + servermsg);
+            cleanServermsg = dissect(servermsg);
 
-            if (servermsg != servermsgOld){
-                System.out.println(this.name + ": " + servermsg); //Op dit moment print hij dus nog iedere keer het nieuwe bericht, maar het is de bedoeling dat dit uiteindelijk bij de klasse komt die ook de zet zet
-                servermsgOld = servermsg;
-            }
+
         }
     }
 
@@ -54,6 +54,84 @@ public class Connection implements Runnable{
 
     public String getservermsg(){
         return (servermsg);
+    }
+
+    private String dissect(String message){
+        if (message.contains("[")){ //als er een [ in zit dan zit er een List in de message
+            message = message.replace(" [ ", "-"); //vervang [ voor - omdat de .split methode niet met { wil splitten
+            message = message.replace("]", ""); //Haal ] aan het einde weg omdat we deze niet nodig hebben
+
+            String[] rawServermsgList = message.split("-"); //je hebt nu een lijst met 2 elementen, het eerste element is de server message en het tweede element is de Map. De Map is nog een String en moet nog omgezet worden.
+
+            serverMsgArrayList = new ArrayList<String>(Arrays.asList(rawServermsgList[1].split(" , "))); //split alle elementen en stopt ze in een ArrayList
+
+            return rawServermsgList[0]; //Hij geeft hier nu alleen de server message door, zoals "SVR PLAYERLIST" maar nog niet de bijbehorende ArrayList waarin in dit geval de players staan.
+            //Wanneer Controller deze message ontvangt en bepaald heeft dat hier een Arraylist bij hoort moet deze de Connection methode getMsgArraylist() aanroepen.
+        }
+        else if (message.contains("{")){ //als er een { in zit dan zit er een Map in de message
+            message = message.replace(" { ", "-"); //vervang { voor - omdat de .split methode niet met { wil splitten
+            message = message.replace("}", ""); //Haal } aan het einde weg omdat we deze niet nodig hebben
+            //System.out.println(message);
+            String[] rawServermsgList = message.split("-"); //je hebt nu een lijst met 2 elementen, het eerste element is de server message en het tweede element is de Map. De Map is nog een String en moet nog omgezet worden.
+
+            String[] pairs = rawServermsgList[1].split(","); //Hier scheid hij alle elementen in de (String) Map van elkaar en stopt ze in een list. Ieder element in de list is dus een key en een value.
+            //System.out.println(pairs[1]);
+
+            for (int i=0;i<pairs.length;i++) { //loop door de lijst met keys en values, maak van 1 String waar een key en een value in staat twee Strings en stop ze in een HashMap
+                String pair = pairs[i];
+                String[] keyValue = pair.split(":");
+                serverMsgHashMap.put(keyValue[0], keyValue[1]);
+            }
+
+            return rawServermsgList[0]; //Hij geeft hier nu alleen de server message door, zoals "SVR GAME MATCH" maar nog niet de bijbehorende HashMap waarin informatie staat als het speltype, naam van de tegenstander, etc.
+            //Wanneer Controller deze message ontvangt en bepaald heeft dat hier een HashMap bij hoort moet deze de Connection methode getMsgHashMap() aanroepen.
+        }
+
+        else { //Zit dus geen List of Map in de message
+            return message;
+        }
+    }
+
+    public HashMap getMsgHashMap() {
+        return serverMsgHashMap;
+    }
+
+    public ArrayList getMsgArrayList(){
+        return serverMsgArrayList;
+    }
+
+    public void selectGame(String game){ //een game selecteren
+        pr.println("subscribe " + game);
+    }
+
+    public void makeMove(int location){ //een zet zetten
+        pr.println("move " + location);
+    }
+
+    public void forfeit(){ //Het hele spel opgeven
+        pr.println("forfeit");
+    }
+
+    public void disconnect(){
+        pr.println("disconnect");
+    }
+
+    public ArrayList getGamelist(){
+        pr.println("get gamelist");
+        return getMsgArrayList();
+    }
+
+    public ArrayList getPlayerlist(){
+        pr.println("get playerlist");
+        return getMsgArrayList();
+    }
+
+    public void challengePlayer(String opponent, String game){
+        pr.println("challenge " + opponent + " " + game);
+    }
+
+    public void acceptChallenge(int challengeNumber){
+        pr.println("challenge accept " + challengeNumber);
     }
 
 
