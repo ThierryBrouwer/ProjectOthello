bf6fdcc9eded230d0ecd8c8810f96f51aa7c048a
Thierry Brouwer <58725453+ThierryBrouwer@users.noreply.github.com>
2021-04-06 22:18:12 +0200

    Update Reversi.java

--- src/Reversi.java
+++ src/Reversi.java
@@ -6,35 +6,39 @@ public class Reversi extends Game {
     private int spelerBeurt;
 
     public Reversi() {
-        spelerBeurt = '1';
+        spelerBeurt = 1;
         Board b = new Board(64);
         board = b.getBoard();
     }
 
     public int blackPoints() {
         int counter = 0;
-        for (int i = 0; i < board.length; i++)
-            if (board[i] == '1')
-                counter ++;
-            return counter;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] == 1) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public int whitePoints() {
         int counter = 0;
-        for (int i = 0; i < board.length; i++)
-            if (board[i] == '2')
-                counter ++;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] == 2) {
+                counter++;
+            }
+        }
         return counter;
     }
 
     // Kijkt of zwart of wit de meeste punten heeft return x als zwart wint, return o als wit wint en return 0 bij gelijkspel.
     public int gameWinner() {
         if (blackPoints() > whitePoints())
-            return '1';
+            return 1;
         if (blackPoints() < whitePoints())
-            return '2';
+            return 2;
         else
-            return '0';
+            return 0;
         }
 
     @Override
