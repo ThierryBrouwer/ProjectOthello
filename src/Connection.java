73f184d69d67a0d0d954b1ae142aa83717756d38
Geert Perton <hippcityofficial@gmail.com>
2021-04-10 13:24:21 +0200

    javaDoc toegevoegd aan de klasse Connection

--- src/Connection.java
+++ src/Connection.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Arrays;
 
+/**
+ * Deze klasse zorgt voor de connectie met de game server door berichten te ontvangen en commando's te sturen.
+ *
+ * @author Geert
+ * @version 10/4/2021
+ */
 public class Connection implements Runnable{
 
     public String name;
@@ -18,17 +24,26 @@ public class Connection implements Runnable{
 
     public PrintWriter pr;
 
+    /**
+     * Constructor voor objecten van de klasse Connection
+     *
+     * @param name  De gebruikersnaam die andere spelers op het netwerk tezien krijgen
+     * @param pr    De PrintWriter
+     */
     public Connection(String name, PrintWriter pr)  {
         this.name = name;
         this.pr = pr;
         serverMsgHashMap = new HashMap<>();
     }
 
+    /**
+     * De methode die de speler inlogt op het netwerk
+     *
+     * @throws IOException wanneer er iets fout gaat met de InputStreamReader
+     */
     public void connect() throws IOException {
         pr.println("login " + this.name);
         pr.flush();
-
-
 
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
@@ -36,24 +51,41 @@ public class Connection implements Runnable{
         while (bf.readLine() != "Exit") {
             servermsg = bf.readLine();
             cleanServermsg = dissect(servermsg);
-
         }
     }
 
+    /**
+     * De methode om een server commando uit te voeren die nog niet is toegevoegd aan deze klasse
+     *
+     * @param commando  Het commando die je wilt uitvoeren in de vorm van een String
+     */
     public void command(String commando){
         pr.println(commando);
         pr.flush();
     }
 
 
+    /**
+     * Geeft het ruwe server bericht zoals deze ontvangen was van de server in de vorm van een String
+     * @return  servermsg het ruwe onverwerkte server bericht
+     */
     public String getservermsg(){
         return (servermsg);
     }
 
+    /**
+     * Geeft alleen het eerste gedeelte van het originele server bericht.
+     * @return  Het verfijnde server bericht zonder de mogelijke list of map waarmee deze origineel werd ontvangen
+     */
     public String getCleanServermsg(){
         return cleanServermsg;
     }
 
+    /**
+     * Split het server bericht van de List of Map en slaat de List als ArrayList op en de Map als Hashmap
+     * @param message is het originele ruwe server bericht
+     * @return is het server bericht zonder de List of Map
+     */
     public String dissect(String message){
         if (message.contains("[")){ //als er een [ in zit dan zit er een List in de message
             message = message.replace("\"", "");
@@ -102,53 +134,93 @@ public class Connection implements Runnable{
             return message;
         }
     }
-
+    /**
+     * Geeft de laatst ontvangen Map terug die de server gestuurd had.
+     * @return De hashmap die in het laatste server bericht zat.
+     */
     public HashMap getMsgHashMap() {
         return serverMsgHashMap;
     }
 
+    /**
+     * Geeft de laatst ontvangen List terug die de server gestuurd had.
+     * @return De ArrayList die in het laatste server bericht zat.
+     */
     public ArrayList getMsgArrayList(){
         return serverMsgArrayList;
     }
 
-    public void selectGame(String game){ //een game selecteren
+    /**
+     * Geeft aan de server door welke game de speler wilt spelen
+     * @param game is het spel die je wilt spelen
+     */
+    public void selectGame(String game){
 
         pr.println("subscribe " + game);
         pr.flush();
     }
 
-    public void makeMove(int location){ //een zet zetten
+    /**
+     * Geeft aan de server door op welke locatie de speler een zet doet
+     * @param location is de locatie van de zet in de vorm van een integer
+     */
+    public void makeMove(int location){
         pr.println("move " + location);
         pr.flush();
     }
 
-    public void forfeit(){ //Het hele spel opgeven
+    /**
+     * Geeft aan de server door dat de speler het hele potje opgeeft
+     */
+    public void forfeit(){
         pr.println("forfeit");
         pr.flush();
     }
 
+    /**
+     * Verbreekt de verbinding met de server
+     */
     public void disconnect(){
         pr.println("disconnect");
         pr.flush();
     }
 
+    /**
+     * Vraagt de server om een lijst met alle spellen die de server ondersteunt
+     */
     public void getGamelist(){
         pr.println("get gamelist");
         pr.flush();
         //return getMsgArrayList();
     }
 
+    /**
+     * vraagt de server om een lijst met alle spelers die online zijn en returned deze lijst als Arraylist
+     *
+     * @return een ArrayList met alle spelers die online zijn
+     */
     public ArrayList getPlayerlist(){
         pr.println("get playerlist");
         pr.flush();
         return getMsgArrayList();
     }
 
+    /**
+     * Een speler uitdagen om een bepaalt spel te spelen
+     *
+     * @param opponent de naam van de speler die je uitdaagt
+     * @param game het spel waarvoor je de tegenstander uitdaagt
+     */
     public void challengePlayer(String opponent, String game){
         pr.println("challenge \"" + opponent + "\" \"" + game);
         pr.flush();
     }
 
+    /**
+     * accepteerd een uitdaging
+     *
+     * @param challengeNumber het nummer van de uitdaging (iedere uitdaging heeft een uniek nummer)
+     */
     public void acceptChallenge(String challengeNumber){
         pr.println("challenge accept " + challengeNumber);
         pr.flush();
