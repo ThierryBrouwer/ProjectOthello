acf39f9926c5a3fb715efb27a719432af8bffcf5
djordyolijve <56109252+djordyolijve@users.noreply.github.com>
2021-04-16 16:40:06 +0200

    Java doc board

--- src/Board.java
+++ src/Board.java
@@ -1,21 +1,40 @@
+/**
+ * de klasse bord, hierin wordt het bord gemaakt, gereset en bijgehouden.
+ *
+ * @author Djordy
+ * @version 15/4/2021
+ */
+
 public class Board {
 
     private int boardsize;
     private int board[];
 
+    /**
+     * de contructor voor klasse Board
+     *
+     * @param boardsize het aantal vakjes dat het bord moet krijgen
+     */
     public Board(int boardsize) {
         this.board = new int[boardsize];
         this.boardsize = boardsize;
         createBoard();
     }
 
-    // Bord voor betreffende spel aanmaken of resetten
+    /**
+     * de methode om het bord aan te maken of te resetten en te printen met lege vakjes (0)
+     */
     public void createBoard() {
         for (int i = 0; i < boardsize; i++) {
             board[i] = 0;
         }
     }
 
+    /**
+     * methode om het bord op te vragen
+     *
+     * @return return hoe het bord eruit ziet
+     */
     public int[] getBoard() {
         return this.board;
     }
